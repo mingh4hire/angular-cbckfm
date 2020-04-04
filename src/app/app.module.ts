875b28df7ce0f16e8b386590b80fc9667d19e1ce
mingh4hire <mingh4hire@gmail.com>
2020-04-04 14:01:21 -0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { MatInputModule } from '@angular/material';
+@NgModule({
+  imports:      [ BrowserModule, FormsModule , MatInputModule],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
